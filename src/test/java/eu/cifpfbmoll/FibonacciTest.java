7d3165ef4a5e120d07517e1ed5771a718b41ed75
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2025-12-16 21:13:19 +0100

    Añadido tests hasta el fibonacci de 8

--- src/test/java/eu/cifpfbmoll/FibonacciTest.java
+++ src/test/java/eu/cifpfbmoll/FibonacciTest.java
@@ -23,6 +23,62 @@ class FibonacciTest {
         assertEquals(1, fibonacci.calculate(1));
 
     }
+    
+    @Test
+
+    void fibonacciOfTwoIsOne() {
+
+        assertEquals(1, fibonacci.calculate(2));
+
+    }
+
+    @Test
+
+    void fibonacciOfThreeIsTwo() {
+
+        assertEquals(2, fibonacci.calculate(3));
+
+    }
+
+    @Test
+
+    void fibonacciOfFourIsThree() {
+
+        assertEquals(3, fibonacci.calculate(4));
+
+    }
+
+    @Test
+
+    void fibonacciOfFiveIsFive() {
+
+        assertEquals(5, fibonacci.calculate(5));
+
+    }
+
+    @Test
+
+    void fibonacciOfSixIsEight() {
+
+        assertEquals(8, fibonacci.calculate(6));
+
+    }
+
+    @Test
+
+    void fibonacciOfSevenIsThirteen() {
+
+        assertEquals(13, fibonacci.calculate(7));
+
+    }
+
+    @Test
+
+    void fibonacciOfEightIsTwentyOne() {
+
+        assertEquals(21, fibonacci.calculate(8));
+
+    }
 
 
 }
