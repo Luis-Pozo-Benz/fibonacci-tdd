79d81ff765cb4f079282c5070810577a80e63c39
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2025-12-16 21:03:14 +0100

    Terminado de añadir los esqueletos

---
+++ src/test/java/eu/cifpfbmoll/FibonacciTest.java
@@ -0,0 +1,19 @@
+package eu.cifpfbmoll;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FibonacciTest {
+
+    private Fibonacci fibonacci = new Fibonacci();
+
+    @Test
+
+    void fibonacciOfZeroIsZero() {
+
+        assertEquals(0, fibonacci.calculate(0));
+
+    }
+
+}
